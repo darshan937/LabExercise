8385e9112471eb0faf5d4374049ee1078935976f
dijac <darshanbhusal41@gmail.com>
2021-06-10 14:31:08 +0545

    2nd program

---
+++ LabOne/qn5.py
@@ -0,0 +1,6 @@
+name = input("enter your name: ")
+age = int(input("enter your age: "))
+
+print("hello my name is " + name + " and i am " + str(age) + " years old")
+print("hello my name is " , name  ," and i am " , age , " years old")
+print(f"hello my name is  {name}  and i am  {age} years old")
