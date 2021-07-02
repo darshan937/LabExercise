873d46164ab5a93b009edd885548b44b9ef74f30
dijac <darshanbhusal41@gmail.com>
2021-07-02 12:44:06 +0545

    temperature conversion

---
+++ lab3/qn2.py
@@ -0,0 +1,6 @@
+'''2.	Write a Python program to convert temperatures to and from celsius, fahrenheit.
+C	= (5/9) * (F - 32)
+'''
+f = int(input("enter the temperature in fahrenhiet: "))
+c = (5/9) * (f-32)
+print(f"{f} fahrenheit temperature is {c} celsius")
